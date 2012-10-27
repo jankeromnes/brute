8435d84fe804df0a00aeff71a4255240c5262f2e
Jan Keromnes <janx@linux.com>
2012-10-28 02:10:00 +0800

    clean up: remove count, i, j, k, debug logs

--- brute.c
+++ brute.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <time.h>
 
 void print(char * t[], int from, int to) {
   int i;
@@ -17,14 +16,10 @@ void swap(char * t[], int i, int j) {
 
 main (int argc, char * argv[]) {
 
-  //clock_t begin, end;
-
   int n = argc - 1;
-  int i = 0, j = 0, k = 0;
   int stack[32] = {0};
   int esp = n - 1;
   int stop = 0;
-  int count = 0;
   int offset = 0;
   int odd = 0;
 
@@ -33,20 +28,11 @@ main (int argc, char * argv[]) {
     return 0;
   }
 
-  //begin = clock();
-
   print(argv, 1, argc);
 
   while (esp > stop) {
 
     if (stack[esp] < esp) {
-
-      //offset = (1 - ((count + stack[esp]) % 2)) * (n - 1 - esp);
-      /*if ((esp < n - 1) && ((count + stack[esp] + 1) % 2)) {
-        offset = 1 + (esp < n - 2) * ((esp + 2) % 2);
-      } else {
-        offset = 0;
-      }*/
 
       odd = (stack[esp] + (esp + 1) * stack[esp - 1]) % 2;
 
@@ -58,29 +44,23 @@ main (int argc, char * argv[]) {
 
       odd = (stack[esp-1] + esp * (esp > 1 ? stack[esp - 2] : 0)) % 2;
 
-      //if (count % 2) {
       if (odd) {
         swap(argv, offset + stack[esp] + 1, offset + stack[esp] + 2);
       } else {
         swap(argv, offset + esp - stack[esp], offset + esp - stack[esp] + 1);
       }
-      printf("[P %d, S %d, C %d, O %d, O %d] - ", esp, stack[esp], count, odd, offset);
       print(argv, 1, argc);
 
       stack[esp]++;
       if (esp <  n - 1) {
-        count += stack[esp];
         esp = n - 1;
       }
+
     } else {
       stack[esp] = 0;
       esp--;
-      count -= stack[esp];
     }
   }
-
-  //end = clock();
-  //printf("Generated in %f seconds\n", (double)(end - begin) / CLOCKS_PER_SEC);
 
   return 0;
 }
