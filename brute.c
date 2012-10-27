d8a15750a0ab7037d265665e57d5ad011db786a5
Jan Keromnes <janx@linux.com>
2012-10-28 02:02:12 +0800

    works

--- brute.c
+++ brute.c
@@ -26,6 +26,7 @@ main (int argc, char * argv[]) {
   int stop = 0;
   int count = 0;
   int offset = 0;
+  int odd = 0;
 
   if (n < 1) {
     printf("usage: %s <sequence>\n", argv[0]);
@@ -41,18 +42,29 @@ main (int argc, char * argv[]) {
     if (stack[esp] < esp) {
 
       //offset = (1 - ((count + stack[esp]) % 2)) * (n - 1 - esp);
-      if ((esp < n - 1) && ((count + stack[esp] + 1) % 2)) {
+      /*if ((esp < n - 1) && ((count + stack[esp] + 1) % 2)) {
+        offset = 1 + (esp < n - 2) * ((esp + 2) % 2);
+      } else {
+        offset = 0;
+      }*/
+
+      odd = (stack[esp] + (esp + 1) * stack[esp - 1]) % 2;
+
+      if ((esp < n - 1) && ((odd + 1) % 2)) {
         offset = 1 + (esp < n - 2) * ((esp + 2) % 2);
       } else {
         offset = 0;
       }
 
-      if (count % 2) {
+      odd = (stack[esp-1] + esp * (esp > 1 ? stack[esp - 2] : 0)) % 2;
+
+      //if (count % 2) {
+      if (odd) {
         swap(argv, offset + stack[esp] + 1, offset + stack[esp] + 2);
       } else {
         swap(argv, offset + esp - stack[esp], offset + esp - stack[esp] + 1);
       }
-      //printf("[n %d, esp %d, s[esp] %d, count %d, offset %d]\n", n, esp, stack[esp], count, offset);
+      printf("[P %d, S %d, C %d, O %d, O %d] - ", esp, stack[esp], count, odd, offset);
       print(argv, 1, argc);
 
       stack[esp]++;
